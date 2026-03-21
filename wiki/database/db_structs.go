7c5d39a7be6c7a1532dc1f5ae8b3f77575586fa4
DeLsonJabberwo <pdhl2006@gmail.com>
2026-03-21 13:57:07 -0500

    fix(wiki): added json tags to db structs

--- wiki/database/db_structs.go
+++ wiki/database/db_structs.go
@@ -7,26 +7,26 @@ import (
 )
 
 type PageInfo struct {
-	UUID			uuid.UUID	`db:"uuid"`
-	Slug			string		`db:"slug"`
-	Name			string		`db:"name"`
-	LastRevisionId	*uuid.UUID	`db:"last_revision_id"`
-	ArchiveDate		*time.Time	`db:"archive_date"`
+	UUID			uuid.UUID	`db:"uuid" json:"uuid"`
+	Slug			string		`db:"slug" json:"slug"`
+	Name			string		`db:"name" json:"name"`
+	LastRevisionId	*uuid.UUID	`db:"last_revision_id" json:"last_revision_id"`
+	ArchiveDate		*time.Time	`db:"archive_date" json:"archive_date"`
 }
 
 type RevInfo struct {
-	UUID		*uuid.UUID	`db:"uuid"`
-	PageId		*uuid.UUID	`db:"page_id"`
-	DateTime	*time.Time	`db:"date_time"`
-	Author		*string		`db:"author"`
-	Slug		string		`db:"slug"`
-	Name		string		`db:"name"`
-	ArchiveDate	*time.Time	`db:"archive_date"`
-	DeletedAt	*time.Time	`db:"deleted_at"`
+	UUID		*uuid.UUID	`db:"uuid" json:"uuid"`
+	PageId		*uuid.UUID	`db:"page_id" json:"page_id"`
+	DateTime	*time.Time	`db:"date_time" json:"date_time"`
+	Author		*string		`db:"author" json:"author"`
+	Slug		string		`db:"slug" json:"slug"`
+	Name		string		`db:"name" json:"name"`
+	ArchiveDate	*time.Time	`db:"archive_date" json:"archive_date"`
+	DeletedAt	*time.Time	`db:"deleted_at" json:"deleted_at"`
 }
 
 type SnapInfo struct {
-	UUID		uuid.UUID	`db:"uuid"`
-	Page		uuid.UUID	`db:"page"`
-	Revision	*uuid.UUID	`db:"revision"`
+	UUID		uuid.UUID	`db:"uuid" json:"uuid"`
+	Page		uuid.UUID	`db:"page" json:"page"`
+	Revision	*uuid.UUID	`db:"revision" json:"revision"`
 }
